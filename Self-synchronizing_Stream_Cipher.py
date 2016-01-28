de634dfaadb42753bda5158ae3033a9e01fef964
tsaoman <BrandonSTsao@gmail.com>
2016-01-27 20:41:00 -0800

    Added variable size

--- Self-synchronizing_Stream_Cipher.py
+++ Self-synchronizing_Stream_Cipher.py
@@ -1,11 +1,13 @@
 #sssc.py
 
-CT = raw_input("Enter CT: ") #Cipher Text (binary string of length 16)
-K = raw_input("Enter Key: ") #Key Stream (binary string of length 16)
+CT = raw_input("Enter CT: ") #Cipher Text (binary string of length n)
+K = raw_input("Enter Key: ") #Key Stream (binary string of length n)
 
-PT = [0] * 18
+n = len(CT)
 
-for i in range(16):
+PT = [0] * (n+2)
+
+for i in range(n):
     if PT[i+1] == 0:
         PT[i+2] = int(CT[i])^int(K[i])^int(PT[i])
     else:
